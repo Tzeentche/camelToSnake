9014c8a085e4219473b130062e0d0bfe0400f758
Tzeentche <ilyasugako87@gmail.com>
2019-12-23 17:24:45 +0300

    Refactored code, ddded 2 methods and realisations to class.

--- src/edu/epam/stringArray/task1/CamelToSnake.java
+++ src/edu/epam/stringArray/task1/CamelToSnake.java
@@ -6,34 +6,72 @@ import java.util.*;
 
 public class CamelToSnake {
 
+//      Declare using values:
+
+    static String[] array;
+    static int arraySize = 0;
+    public static String fewWords;
+
     public static void main(String[] args) {
 
-        //      Declare using values
+        CamelToSnake giveMeSnakeCase = new CamelToSnake();
+        giveMeSnakeCase.inputString();
+    }
 
-        int buffer = 0;
-        int size;
-        String[] array;
+    public String inputString() {
+
+        Scanner inputString = new Scanner(System.in);
 
         try {
 
+//      Input the origin String:
 
-        } catch(InputMismatchException e1) {
+            System.out.println("Please, enter a some string, consists with few words:");
+            fewWords = inputString.nextLine();
+            inputString.close();
+
+        } catch (InputMismatchException e1) {
 
             System.out.println("Entered value is not expected type.");
 
-        } catch(ArrayIndexOutOfBoundsException e2) {
+        } catch (ArrayIndexOutOfBoundsException e2) {
 
             System.out.println("The size of the array out of bounds.");
         }
-    }
 
-    String arrayNames() {
+        System.out.println("RakaMakaFo!!!! " + fewWords);
+
+        arrayConverter(fewWords);
 
         return "";
     }
 
-    String camelToSnake() {
+    public String arrayConverter(String inputString) {
 
+        arraySize = fewWords.length();
+        array = new String[arraySize];
+        String[] charsArray = inputString.split("");
+
+        System.out.println("Our array is: ");
+        for(int counter = 0; counter < arraySize; counter++) {
+
+            System.out.println(charsArray[counter]);
+        }
+
+        camelToSnake(charsArray);
+        return "";
+    }
+
+    public String camelToSnake(String[] mutantString) {
+
+        arraySize = 1 + fewWords.length();
+        for(int counter = 0; counter < arraySize; counter++) {
+
+            if(1 == 1) {
+
+                
+            }
+        }
         return "";
     }
 
