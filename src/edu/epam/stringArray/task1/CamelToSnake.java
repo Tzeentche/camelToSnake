e07f868a6dd684cd56ac3b4378578d154f474d4f
Tzeentche <ilyasugako87@gmail.com>
2019-12-23 14:19:28 +0300

    Added main methods to class.

--- src/edu/epam/stringArray/task1/CamelToSnake.java
+++ src/edu/epam/stringArray/task1/CamelToSnake.java
@@ -2,5 +2,48 @@ package edu.epam.stringArray.task1;
 
 //      Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
 
+import java.util.*;
+
 public class CamelToSnake {
+
+    public static void main(String[] args) {
+
+        //      Declare using values
+
+        int buffer = 0;
+        int size;
+        String[] array;
+
+        try {
+
+
+        } catch(InputMismatchException e1) {
+
+            System.out.println("Entered value is not expected type.");
+
+        } catch(ArrayIndexOutOfBoundsException e2) {
+
+            System.out.println("The size of the array out of bounds.");
+        }
+    }
+
+    String arrayNames() {
+
+        return "";
+    }
+
+    String camelToSnake() {
+
+        return "";
+    }
+
+    String outputResult()
+    {
+
+        return "";
+
+    }
+
+
+
 }
