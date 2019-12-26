cf6206a6717de2d194d1fd90ca8d38551bc49941
Tzeentche <ilyasugako87@gmail.com>
2019-12-26 16:09:22 +0300

    Add method's structure to develop task 3.1.2.

--- src/edu/epam/stringArray/task2/WordsChanger.java
+++ src/edu/epam/stringArray/task2/WordsChanger.java
@@ -1,4 +1,32 @@
 package edu.epam.stringArray.task2;
 
+//      Замените в строке все вхождения 'word' на 'letter'
+
+import java.util.*;
+
 public class WordsChanger {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void inputString() {
+
+
+    }
+
+    private void filterString() {
+
+
+    }
+
+    private void changeString() {
+
+
+    }
+
+    private void outputString() {
+
+        
+    }
 }
