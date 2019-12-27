0d480f0a7f8ee4724c393e1e03d67ea0618da5b6
Tzeentche <ilyasugako87@gmail.com>
2019-12-27 15:08:51 +0300

    Add comments to task 3.1.4.

--- src/edu/epam/stringArray/task4/FiguresFinder.java
+++ src/edu/epam/stringArray/task4/FiguresFinder.java
@@ -9,6 +9,8 @@ public class FiguresFinder {
 
     public static void main(String[] args) {
 
+//        Create class example:
+
         FiguresFinder figuresFinder = new FiguresFinder();
 
         figuresFinder.usersInput();
@@ -16,10 +18,16 @@ public class FiguresFinder {
 
     private void usersInput() {
 
+//        Declare local variables:
+
         Scanner usersSentence = new Scanner(System.in);
         String sentenceHolder = "";
 
+//        (I recommend to use next one sentence: "1.1Words5 w0rds 13! 1 -0 0.2  3\,4"):
+
         System.out.println("Please, enter some sentence with numbers!");
+
+//        Check user's sentence:
 
         try {
 
@@ -37,9 +45,11 @@ public class FiguresFinder {
 
     private void figuresCounter(String stringsArray) {
 
+//        Declare local variables:
+
         int result = 0;
         int arraySize = stringsArray.length();
-        Pattern pattern = Pattern.compile("[0.0-9.9]+");
+        Pattern pattern = Pattern.compile("[0.0-99]+");
         Matcher match = pattern.matcher(stringsArray);
 
         for (int count = 0; count < arraySize; count++) {
@@ -55,6 +65,8 @@ public class FiguresFinder {
 
     private void outputResult(int finalStep) {
 
-        System.out.println("Our sentence consists " + finalStep + " figures. YEEEEEEE!!!");
+//        Output result:
+
+        System.out.println("Our sentence consists " + finalStep + " figures!");
     }
 }
