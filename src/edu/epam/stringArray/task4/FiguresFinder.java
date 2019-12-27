53031e53fec8f4fd3655635dc983bf8ca765980f
Tzeentche <ilyasugako87@gmail.com>
2019-12-27 12:48:37 +0300

    Creatre logical structure of methods for task 3.1.4.

---
+++ src/edu/epam/stringArray/task4/FiguresFinder.java
@@ -0,0 +1,31 @@
+package edu.epam.stringArray.task4;
+
+//  В строке найти количество чисел.
+
+import java.util.*;
+import java.util.regex.*;
+
+public class FiguresFinder {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void usersInput() {
+
+    }
+
+    private void messageSplitter() {
+
+    }
+
+    private void figuresCounter() {
+
+    }
+
+    private void outputResult() {
+        
+    }
+}
+
+
