987ca5a83105de1e468ebac8648f9c1689bf23fe
Tzeentche <ilyasugako87@gmail.com>
2019-12-27 12:08:07 +0300

    Started to develop solution to task 3.1.3. Create structure of methods.

---
+++ src/edu/epam/stringArray/task3/NumbersFinder.java
@@ -0,0 +1,27 @@
+package edu.epam.stringArray.task3;
+
+//  В строке найти количество цифрфр.
+
+import java.util.*;
+
+public class NumbersFinder {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void messageReceiver() {
+
+
+    }
+
+    private void figuresCounter() {
+
+
+    }
+
+    private void counterOutputter() {
+
+        
+    }
+}
