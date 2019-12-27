08764751e9d5182e492c32f4c363714dc2d0a17f
Tzeentche <ilyasugako87@gmail.com>
2019-12-27 11:57:43 +0300

    Finished developing logic and methods to task 3.1.2. Now code is ready to run.

--- src/edu/epam/stringArray/task2/WordsChanger.java
+++ src/edu/epam/stringArray/task2/WordsChanger.java
@@ -8,25 +8,40 @@ public class WordsChanger {
 
     public static void main(String[] args) {
 
+        WordsChanger wordOnLetter = new WordsChanger();
+        wordOnLetter.inputString();
     }
 
     private void inputString() {
 
+        String messageWithWords = "";
+        Scanner inputMessage = new Scanner(System.in);
 
+        try {
+
+            System.out.println("Please< enter your message with words \"Word\": ");
+            messageWithWords = inputMessage.nextLine();
+            inputMessage.close();
+
+        } catch (InputMismatchException ex1) {
+
+            System.out.println("Missmatch excaption!");
+        }
+
+        System.out.println("Your message is: " + messageWithWords);
+
+        filterString(messageWithWords);
     }
 
-    private void filterString() {
+    private void filterString(String incomingMessage) {
 
+        String messageConverter = incomingMessage.replaceAll("[Ww][Oo][Rr][Dd]", "letter");
 
+        outputter(messageConverter);
     }
 
-    private void changeString() {
+    private void outputter (String outputMessage) {
 
-
-    }
-
-    private void outputString() {
-
-
+        System.out.println("Now our message is: " + outputMessage);
     }
 }
