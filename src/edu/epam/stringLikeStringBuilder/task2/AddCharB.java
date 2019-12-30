5cf66ad3bc18e7499ff8d24bd5ed9cf2acd82080
Tzeentche <ilyasugako87@gmail.com>
2019-12-30 16:34:15 +0300

    Started to develop project< add logical structure to task 3.2.2.

---
+++ src/edu/epam/stringLikeStringBuilder/task2/AddCharB.java
@@ -0,0 +1,31 @@
+package edu.epam.stringLikeStringBuilder.task2;
+
+//      В строке вставить после каждого символа 'а' символ 'b'
+
+import java.util.*;
+import java.util.regex.*;
+
+public class AddCharB {
+
+    public static void main(String[] args) {
+
+        AddCharB makeExpressionGreatAgain = new AddCharB();
+        makeExpressionGreatAgain.expressionReceiver();
+    }
+
+    private void expressionReceiver() {
+        
+
+    private void expressionConverter(String expressionToConvert) {
+
+
+    }
+
+    private void addedToB() {
+
+    }
+
+    private void resultOutput() {
+
+    }
+}
