d5a06286e26a3a6aa35583373adac616f507524e
Tzeentche <ilyasugako87@gmail.com>
2019-12-30 17:05:42 +0300

    Add code and some solutions to task 3.2.2.

--- src/edu/epam/stringLikeStringBuilder/task2/AddCharB.java
+++ src/edu/epam/stringLikeStringBuilder/task2/AddCharB.java
@@ -2,6 +2,8 @@ package edu.epam.stringLikeStringBuilder.task2;
 
 //      В строке вставить после каждого символа 'а' символ 'b'
 
+//      A see a line of cars and they are all painted black
+
 import java.util.*;
 import java.util.regex.*;
 
@@ -14,11 +16,47 @@ public class AddCharB {
     }
 
     private void expressionReceiver() {
-        
+
+        String usersSentence = "";
+        Scanner insertData = new Scanner(System.in);
+
+        System.out.println("Please, enter your sentence with many chars 'a': ");
+        try {
+
+            usersSentence = insertData.nextLine();
+            insertData.close();
+
+        } catch(InputMismatchException ex1) {
+
+            System.out.println("It's really impossible, but you did it: you broke everything!!!");
+        }
+
+        expressionConverter(usersSentence);
+    }
 
     private void expressionConverter(String expressionToConvert) {
 
+        String[] charsArray = expressionToConvert.split("");
+        int[] charsPosition = new int[charsArray.length];
 
+        System.out.println("Chars length is: " + charsArray.length);
+
+        for (int counter = 0; counter < charsArray.length; counter++) {
+
+            if(charsArray[counter] == "a") {
+
+                charsPosition[counter] = counter;
+            }
+        }
+
+        System.out.print("Char \"A\" we find at the next positions: ");
+
+        for (int counter = 0; counter < charsPosition.length; counter++) {
+
+            System.out.print(charsPosition[counter] + ", ");
+        }
+
+        System.out.println("");
     }
 
     private void addedToB() {
