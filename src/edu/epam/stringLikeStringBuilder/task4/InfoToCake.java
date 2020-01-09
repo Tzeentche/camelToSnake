b934ae0de3cc5cc8b1d5bc0ef9f8f54994569a6d
Tzeentche <ilyasugako87@gmail.com>
2020-01-09 17:14:51 +0300

    Finished to develop code for task 3.2.4.

--- src/edu/epam/stringLikeStringBuilder/task4/InfoToCake.java
+++ src/edu/epam/stringLikeStringBuilder/task4/InfoToCake.java
@@ -2,23 +2,35 @@ package edu.epam.stringLikeStringBuilder.task4;
 
 //      С помощью функции копирования и операции конкатенации составить из частей слова "информатика" слово "торт"
 
-import java.util.*;
-
 public class InfoToCake {
+
+    StringBuffer source = new StringBuffer("информатика");
+    char[] firstPart = new char[source.length()];
 
     public static void main(String[] args) {
 
+        InfoToCake infoToCake = new InfoToCake();
+        infoToCake.copyFunc();
     }
 
     private void copyFunc() {
 
+            firstPart[0] = source.charAt(3);
+            firstPart[1] = source.charAt(4);
+            firstPart[2] = source.charAt(7);
+
+            concatOper();
     }
 
     private void concatOper() {
 
+        String resultIs = ("" + firstPart[2] + firstPart[0] + firstPart[1] + firstPart[2]);
+
+        cakeOutput(resultIs);
     }
 
-    private void cakeOutput() {
+    private void cakeOutput(String sentence) {
 
+        System.out.println("Result is: " + sentence);
     }
 }
