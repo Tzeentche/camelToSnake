c7a6d4dbff33116452b0c638c8075728533f0e67
Tzeentche <ilyasugako87@gmail.com>
2020-01-14 10:44:26 +0300

    Started developing logical part and code to task 3.2.6.

--- src/edu/epam/stringLikeStringBuilder/task6/DoubleChar.java
+++ src/edu/epam/stringLikeStringBuilder/task6/DoubleChar.java
@@ -9,6 +9,31 @@ public class DoubleChar {
 
     public static void main(String[] args) {
 
+        DoubleChar doubleChar = new DoubleChar();
+        doubleChar.insertSentence();
+    }
+
+    private void insertSentence() {
+
+        String sentence = "";
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please, enter your sentence: ");
+        try {
+
+            sentence = scan.nextLine();
+            scan.close();
+
+        } catch (InputMismatchException ex1) {
+
+            System.out.println("Something going wrong!...");
+        }
+
+        sentenceDoubler(sentence);
+    }
+
+    private void sentenceDoubler(String expression) {
+
         
     }
 }
