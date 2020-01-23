ce02229163639643598a9d1c8d25f4a417020cc3
Tzeentche <ilyasugako87@gmail.com>
2020-01-23 14:50:52 +0300

    Add comments to task 3.2.9.

--- src/edu/epam/stringLikeStringBuilder/task9/CharsCounter.java
+++ src/edu/epam/stringLikeStringBuilder/task9/CharsCounter.java
@@ -9,16 +9,22 @@ public class CharsCounter {
 
     public static void main(String[] args) {
 
+//        Create class example:
+
         CharsCounter charsCounter = new CharsCounter();
         charsCounter.charsInsert();
     }
 
     private void charsInsert() {
 
+//        Declare local variables:
+
         String usersSentence = "";
         Scanner scn = new Scanner(System.in);
 
         System.out.println("Let's enter sentence:");
+
+//        Try to find exceptions:
 
         try {
 
@@ -35,6 +41,8 @@ public class CharsCounter {
 
     private void textSplitter(String usersText) {
 
+//        Split user's expression;
+
         String[] textArray = usersText.split("[.]");
 
         charsCounter(textArray);
@@ -42,9 +50,13 @@ public class CharsCounter {
 
     private void charsCounter(String[] innerExpression) {
 
+//        Declare local variables:
+
         int lowChars = 0, highChars = 0;
 
         for(int counter = 0; counter < innerExpression.length; counter++) {
+
+//            Compare user's text with patterns:
 
             Pattern lowPattern = Pattern.compile("[a-z]");
             Matcher firstMatch = lowPattern.matcher(innerExpression[counter]);
@@ -68,6 +80,8 @@ public class CharsCounter {
 
     private void resultOutput(int lowChars, int highChars) {
 
+//        Output result:
+        
         System.out.println("Count of low case chars is " + lowChars + ", and count of high case is " + highChars);
     }
 }
