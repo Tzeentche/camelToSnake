cec5932945c2d19b2253b0d1d8e71557d7e5e5de
Tzeentche <ilyasugako87@gmail.com>
2020-01-23 15:25:16 +0300

    Started to develop task 3.2.10.

---
+++ src/edu/epam/stringLikeStringBuilder/task10/StringX.java
@@ -0,0 +1,11 @@
+package edu.epam.stringLikeStringBuilder.task10;
+
+//      Строка X состоит из нескольких предложений, каждое из которых кончается точкой, восклицательным или вопросительным знаком.
+//      Определить количество предложений в строке X.
+
+public class StringX {
+
+    public static void main(String[] args) {
+        
+    }
+}
